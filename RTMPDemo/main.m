6b8b725608c19d507e2d5a405ab89d4200fbafaa
Janven <janven@localhost>
2016-07-28 17:05:25 +0800

    Initial Commit

---
+++ RTMPDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RTMPDemo
+//
+//  Created by Janven on 16/7/28.
+//  Copyright © 2016年 Janven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
